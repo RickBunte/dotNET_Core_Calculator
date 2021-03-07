69fb2c68149ed9d554476ce14120672082610ccb
Rick Bunte <rickbunte2000@gmail.com>
2021-03-07 19:33:32 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotNET_Core_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
